4b96abcb070a1a8ee7b5220939dd767e5ed32b10
Avi Flombaum <avi@flombaum.com>
2017-11-12 20:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ BoardMember.prototype.veto = function(){
     returns `No, I must disagree`
 }
   
-BoardMember.prototype.veto = function(){
-    returns `No, I must disagree`
+BoardMember.prototype.approve = function(){
+    returns `You can do that!`
 }
 
 BoardMember.prototype.veto = function(){
