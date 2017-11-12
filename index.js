17223743cea8d330ee87cdb3ac2aa643cd196cf2
Avi Flombaum <avi@flombaum.com>
2017-11-12 20:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ BoardMember.prototype.doCharity = function(){
     returns `I like to help people.`
 }
 
-BoardMember.prototype.veto = function(){
-    returns `No, I must disagree`
+BoardMember.prototype.releasePressStatement = function(){
+    returns `You will see great things from Scuber.`
+}
+
+BoardMember.prototype.sayHi = function(){
+    returns `You will see great things from Scuber.`
 }
 
