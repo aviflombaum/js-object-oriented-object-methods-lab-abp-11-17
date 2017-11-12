aeee4ebe5d5aafffc8c08a343499df2a84c363c4
Avi Flombaum <avi@flombaum.com>
2017-11-12 20:42:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ BoardMember.prototype.releasePressStatement = function(){
 }
 
 BoardMember.prototype.sayHi = function(){
-    returns `Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
+    returns `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 
