0c67e2f2e40253b4f1e4834d59713ab027592d2d
Avi Flombaum <avi@flombaum.com>
2017-11-12 20:42:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ function BoardMember(name, homeState, training){
 }
 
 BoardMember.prototype.veto = function(){
-    returns `No, I must disagree`
+    return `No, I must disagree`
 }
   
 BoardMember.prototype.approve = function(){
-    returns `You can do that!`
+    return `You can do that!`
 }
 
 BoardMember.prototype.doCharity = function(){
-    returns `I like to help people.`
+    return `I like to help people.`
 }
 
 BoardMember.prototype.releasePressStatement = function(){
-    returns `You will see great things from Scuber.`
+    return `You will see great things from Scuber.`
 }
 
 BoardMember.prototype.sayHi = function(){
-    returns `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 
